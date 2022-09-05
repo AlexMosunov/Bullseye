254e846f0867a5ded00aa74137c9eae53a4c36ea
Oleksii Mosunov <o.mosunov@mobidev.biz>
2022-09-05 14:06:46 +0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Alex Mosunov on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
